79fe4720c0b9f906f5626d1556eb096c086163a9
Cesaru <cesaru.desu@gmail.com>
2020-11-19 16:00:46 -0500

    final

--- App.js
+++ App.js
@@ -1,42 +1,98 @@
-import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView, StatusBar} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet, View, Text, SafeAreaView, StatusBar,
+  Button
+} from 'react-native';
 import Form from './src/components/Form';
+import Footer from './src/components/Footer';
+import ResultCalculation from './src/components/ResultCalculation';
 import colors from './src/utils/colors';
 
 
 export default function App() {
+  const [capital, setCapital] = useState(null);
+  const [interest, setInterest] = useState(null);
+  const [months, setMonths] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [errorMessague, setErrorMessague] = useState('')
+
+  // función flecha: => {
+  useEffect(() => {
+    // si esta con contenido
+    
+    // if (capital && interest && months) {
+    //   calculate();
+    // } else {
+    //   reset();
+    // }
+    // sintaxis simplificada solo valida para un sola condición
+    if (capital && interest && months) calculate();
+    else reset();
+    
+  }, [capital, interest, months])
+  // raid = []
+
+  const calculate = () => {
+    reset();
+    if (!capital) {
+      setErrorMessague("Añade la cantidad que quieres solicitar");
+    } else if (!interest) {
+      setErrorMessague("Añade el interes del prestamo");
+    } else if (!months) {
+      setErrorMessague("Seleccióna los meses a pagar");
+    } else {
+      const i = interest / 100;
+      const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+      // console.log(fee.toFixed(2).replace('.', ","));
+      setTotal({
+        monthlyFee: fee.toFixed(2).replace('.', ","), // pago mensual
+        totalPayable: (fee * months).toFixed(2).replace('.', ",") // pago total
+      });
+    }
+  };
+
+  const reset = () => {
+    setErrorMessague("");
+    setTotal(null)
+  }
+
   return(
     <>
     <StatusBar barStyle="light-content" />
     <SafeAreaView style={styles.SafeArea}>
+    <View style={styles.background}/>
       <Text style={styles.titleApp}>
         Cotizador de prestamos
       </Text>
-    <Form />
+    <Form setCapital={setCapital} setInterest={setInterest} setMonths={setMonths}/>
     </SafeAreaView>
 
-    {/* <View>
-      <Text>
-        Resultado
-      </Text>
-    </View>
+    <ResultCalculation 
+    capital={capital}
+    interest={interest}
+    months={months}
+    total={total}
+    errorMessague={errorMessague} 
+    />
 
-    <View>
-      <Text>
-        Footer
-      </Text>
-    </View> */}
+    <Footer calculate={calculate} />
     </>
   );
 }
 
 const styles = StyleSheet.create({
   SafeArea: {
+    height: 290,
+    alignItems: "center"
+  },
+  background: {
     backgroundColor: colors.PRIMARY_COLOR,
     height: 200,
+    width: '100%',
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
-    alignItems: "center"
+    position: 'absolute',
+    zIndex: -1,
   },
   titleApp:{
     fontSize: 25,
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,19 +3,36 @@ import {StyleSheet, TextInput, View} from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import colors from '../utils/colors';
 
-export default function Form() {
+export default function Form(props) {
+    const {setCapital, setInterest, setMonths} = props;
     return(
         <View style={styles.viewForm}>
+            
             <View style={styles.viewInputs}>
-                <TextInput style={styles.input} placeholder="Cantidad a pedir" keyboardType="numeric" />
-                <TextInput style={[styles.input, styles.inputPercentage]} placeholder="Interes %" keyboardType="numeric" />
+                <TextInput 
+                style={styles.input} 
+                placeholder="Cantidad a pedir" 
+                keyboardType="numeric" 
+                onChange={e => setCapital(e.nativeEvent.text) }
+                />
+                <TextInput 
+                style={[styles.input, styles.inputPercentage]} 
+                placeholder="Interes %" 
+                keyboardType="numeric" 
+                onChange={e => setInterest(e.nativeEvent.text) }
+                />
             </View>
-            <RNPickerSelect
-                onValueChange={(value) => console.log(value)}
+            <RNPickerSelect style={picketSelectStyles}
+                onValueChange={(value) => setMonths(value)}
+                placeholder={{
+                    label: 'Selecciona los plazos..',
+                    value: null
+                }}
                 items={[
-                    { label: 'Football', value: 'football' },
-                    { label: 'Baseball', value: 'baseball' },
-                    { label: 'Hockey', value: 'hockey' },
+                    { label: '3 meses', value: 3 },
+                    { label: '6 meses', value: 6 },
+                    { label: '12 meses', value: 12 },
+                    { label: '24 meses', value: 24 },
                 ]}
             />
         </View>
@@ -25,7 +42,7 @@ export default function Form() {
 const styles = StyleSheet.create({
     viewForm: {
         position: "absolute",
-        bottom: -90,
+        bottom: 0,
         width: "85%",
         paddingHorizontal: 50,
         backgroundColor: colors.PRIMARY_COLOR_DARK,
@@ -55,4 +72,31 @@ const styles = StyleSheet.create({
         width: "40%",
         marginLeft: 5,
     }
+});
+
+const picketSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30,
+        backgroundColor: '#fff',
+        marginLeft: -5,
+        marginRight: -5,
+      },
+      inputAndroid: {
+        fontSize: 16,
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        borderWidth: 0.5,
+        borderColor: 'grey',
+        borderRadius: 8,
+        color: 'black',
+        paddingRight: 30,
+        backgroundColor: '#fff',
+      },
 });
